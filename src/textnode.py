6415e87e60227ee994eab232dcaeda3febc173c0
Rpgale43 <rpgale43@gmail.com>
2025-05-15 17:45:48 -0500

    conversion of text nodes to leaf nodes function added

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum 
+from htmlnode import LeafNode
 
 class TextType(Enum):
     NORMAL_TEXT = "normal"
@@ -22,3 +23,20 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.NORMAL_TEXT:
+            return LeafNode(tag=None, value=text_node.text)
+        case TextType.BOLD_TEXT:
+            return LeafNode(tag="b", value=text_node.text)
+        case TextType.ITALIC_TEXT:
+            return LeafNode(tag="i", value=text_node.text)
+        case TextType.CODE_TEXT:
+            return LeafNode(tag="code", value=text_node.text)
+        case TextType.LINK_TEXT:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        case TextType.IMAGE_TEXT:
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("Not a standard text type")
